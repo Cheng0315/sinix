2db4f15e141b4182cdb710ec0198d0eeb412c89f
Cheng Vue <cvu3@ucsc.edu>
2019-01-13 18:15:29 +0000

    change models_names to model_names

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -4,12 +4,12 @@ class ProjectsController < ApplicationController
     erb :'projects/create_project/number_of_models'
   end
 
-  post "/projects/new/models-names" do
+  post "/projects/new/model-names" do
     @num_of_models = params[:num_of_models].to_i
-    erb :'projects/create_project/models_name'
+    erb :'projects/create_project/model_names'
   end
 
   post "/projects" do
-    "hello"
+
   end
 end
