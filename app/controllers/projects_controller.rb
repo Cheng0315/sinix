099a8d7145e5fed9f28f49ebd2f00584b1089994
Cheng Vue <cvu3@ucsc.edu>
2019-01-13 17:50:53 +0000

    add create project form and create project routes

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,3 +1,15 @@
 class ProjectsController < ApplicationController
 
+  get "/projects/new" do
+    erb :'projects/create_project/number_of_models'
+  end
+
+  post "/projects/new/models-names" do
+    @num_of_models = params[:num_of_models].to_i
+    erb :'projects/create_project/models_name'
+  end
+
+  post "/projects" do
+    "hello"
+  end
 end
